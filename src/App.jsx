bec59d2d02cb9a06b0d1fc27a12f5edc04cec28d
EMMANUEL CORDEIRO MONTE <EMMANUEL0899@DIGITALCOLLEGE.LOCAL>
2024-07-04 16:24:56 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./pages"
+import Cadastro from "./pages/Cadastro"
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Login/>} />
+        <Route path="/cadastro" element={<Cadastro/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Cadastro/index.jsx
@@ -0,0 +1,7 @@
+import './style.css';
+
+export default function Cadastro(){
+    return (
+        <h1>Home</h1>
+    )
+}
